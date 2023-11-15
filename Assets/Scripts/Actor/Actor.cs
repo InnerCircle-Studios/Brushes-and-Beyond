cd107f952c3e9b30d1f6137bafbc7a1f05244509
M0rga-n <verburgnoah@gmail.com>
2023-11-15 12:04:02 +0100

    Partaly added Actor and Player

---
+++ Assets/Scripts/Actor/Actor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public abstract class Actor : MonoBehaviour
+{
+    public Actor()
+    {
+        _AttributeManager = new AttributeManager();
+
+        _Combat = new Combat();
+    }
+
+    public abstract void Start();
+
+    public abstract void Update();
+
+    public abstract void HandleMeleeAttack();
+
+    public abstract void HandleRangedAttack();
+
+    public IAttrubuteManager GetAttrubuteManager()
+    {
+        return _AttributeManager;
+    }
+
+    public  ICombat GetCombat()
+    {
+        return _Combat;
+    }
+
+    private IAttrubuteManager _AttributeManager;
+
+    private ICombat _Combat;
+}
