eecf9b33864581f7a38e47ff6b7e6f6db19f95a4
M0rga-n <verburgnoah@gmail.com>
2023-11-22 12:32:57 +0100

    Added input handeling to InputHandler and added seperate events to Event Manager. Changed function to use the new EventBus.

--- Assets/Scripts/EventManager/EventManager.cs
+++ Assets/Scripts/EventManager/EventManager.cs
@@ -1,39 +1,16 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class EventManager : MonoBehaviour {
     private EventManager() { }
 
-    public void OnMove(InputAction.CallbackContext context) {
-        OnMoveEvent(context.ReadValue<Vector2>());
+    public void OnDeath(bool isDeath) 
+    {
+        EventBus.TriggerEvent(EventBusEvents.EventName.DEATH_EVENT, isDeath);
     }
 
-    public void OnAttack(InputAction.CallbackContext context) {
-        OnAttackEvent(context.ReadValueAsButton());
-    }
-
-    public void OnRun(InputAction.CallbackContext context) {
-        OnRunEvent(context.ReadValueAsButton());
-    }
-
-    public void OnDash(InputAction.CallbackContext context) {
-        OnDashEvent();
-    }
-
-    public void OnInteract(InputAction.CallbackContext context) {
-        OnInteractEvent(context.ReadValueAsButton());
-    }
-
-    public void OnShow(InputAction.CallbackContext context) {
-        OnShowEvent(context.ReadValueAsButton());
-    }
-
-    public void OnDeath(bool isDeath) {
-        OnDeathEvent(isDeath);
-    }
-
-    public void OnDialogue(bool isDialogueTrue) {
-        OnDialogueEvent(isDialogueTrue);
+    public void OnDialogue(bool isDialogueTrue) 
+    {
+        EventBus.TriggerEvent(EventBusEvents.EventName.DIALOGUE_EVENT, isDialogueTrue);
     }
 
     static public EventManager GetEventManager() {
@@ -43,25 +20,6 @@ public class EventManager : MonoBehaviour {
 
         return _eventManager;
     }
-
-    public delegate void Action();
-    public delegate void Bool(bool isDeath);
-    public delegate void MoveEvent(Vector2 move);
-    public event MoveEvent OnMoveEvent;
-
-    public event Bool OnAttackEvent;
-
-    public event Bool OnRunEvent;
-
-    public event Action OnDashEvent;
-
-    public event Bool OnInteractEvent;
-
-    public event Bool OnShowEvent;
-
-    public event Bool OnDeathEvent;
-
-    public event Bool OnDialogueEvent;
 
     private static EventManager _eventManager;
 }
