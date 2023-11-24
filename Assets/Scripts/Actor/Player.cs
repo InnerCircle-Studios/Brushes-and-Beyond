319555d8fa6f47f128ab0a7ce7d1cf544c62391c
Justwouter <wouter2312@gmail.com>
2023-11-24 12:00:33 +0100

    Added death, Added an IsAlive() Function

--- Assets/Scripts/Actor/Player.cs
+++ Assets/Scripts/Actor/Player.cs
@@ -1,33 +1,28 @@
 using UnityEngine;
 
-public class Player : Actor
-{
-    public override void Start()
-    {
+public class Player : Actor {
+    public override void Start() {
         _PlayerStateMachine = new PlayerStateMachine(this);
     }
 
-    public override void Update()
-    {
+    public override void Update() {
         _PlayerStateMachine.GetCurrentState().UpdateState();
-        Debug.Log(_PlayerStateMachine._CurrentDirection);
+        if (!GetAttrubuteManager().IsAlive()) {
+            OnDeath();
+        }
     }
 
-    public override void HandleMeleeAttack()
-    {
-        foreach (Actor hits in GetCombat().MeleeAttack(GetRigidBody().position , 1.5f, "Enemy"))
-        {
+    public override void HandleMeleeAttack() {
+        foreach (Actor hits in GetCombat().MeleeAttack(GetRigidBody().position, 1.5f, "Enemy")) {
             hits.GetAttrubuteManager().ApplyDamage(GetAttrubuteManager().GetAttributes().Damage);
         }
     }
 
-    public override void HandleRangedAttack() 
-    {
-        
+    public override void HandleRangedAttack() {
+
     }
 
-    public void OnDeath()
-    {
+    public void OnDeath() {
         EventBus.TriggerEvent(EventBusEvents.EventName.DEATH_EVENT, true);
     }
 
