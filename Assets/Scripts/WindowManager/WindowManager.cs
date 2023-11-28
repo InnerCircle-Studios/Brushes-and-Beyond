9ffc3171027093d81652d95efc56d796cf8d053d
Justwouter <wouter2312@gmail.com>
2023-11-28 14:27:18 +0100

    Added a toggle function for windows Added null safety to linq queries Mapped some more buttons to the correct functions

--- Assets/Scripts/WindowManager/WindowManager.cs
+++ Assets/Scripts/WindowManager/WindowManager.cs
@@ -23,14 +23,20 @@ public class WindowManager : MonoBehaviour {
 
     // static screens
     public void ShowScreen(string screen) {
-        staticWindows.First(e => e.Name == screen).Element.SetActive(true);
+        staticWindows?.First(e => e.Name == screen).Element.SetActive(true);
     }
     public void HideScreen(string screen) {
-        staticWindows.First(e => e.Name == screen).Element.SetActive(false);
+        staticWindows?.First(e => e.Name == screen).Element.SetActive(false);
     }
 
-    public void ClearScreen(string[] excludedScreens){
-        staticWindows.FindAll(e => !excludedScreens.Contains(e.Name)).ForEach(e => HideScreen(e.Name));
+    public void ToggleScreen(string screen) {
+        GameObject window = staticWindows?.First(e => e.Name == screen).Element;
+        window.SetActive(!window.activeSelf);
+
+    }
+
+    public void ClearScreen(string[] excludedScreens) {
+        staticWindows?.FindAll(e => !excludedScreens.Contains(e.Name)).ForEach(e => HideScreen(e.Name));
     }
 
 
