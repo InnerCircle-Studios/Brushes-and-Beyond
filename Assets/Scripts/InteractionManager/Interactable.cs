ffdb08b11d975d05376ae34225ed46fb3bec0b57
Justwouter <wouter2312@gmail.com>
2023-12-11 12:07:46 +0100

    Added interactionPoints

--- Assets/Scripts/InteractionManager/Interactable.cs
+++ Assets/Scripts/InteractionManager/Interactable.cs
@@ -3,4 +3,9 @@ using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour {
     public UnityEvent OnEventTrigger = new();
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, .3f);
+    }
 }
