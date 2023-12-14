c5baf963d155c2554051f594ae0a22943390547e
Noah Verburg <verburgnoah@gmail.com>
2023-12-14 13:26:32 +0100

    Paint Tube Actor

---
+++ Assets/Scripts/Actor/PaintTube.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PaintTube : Actor {
+    public override void Start() 
+    {
+        _Player = GetComponent<Player>();
+        
+        _PaintTubeStateMachine = new PaintTubeStateMachine(this, _Player);
+    }
+
+    public override void Update() {
+        _PaintTubeStateMachine.GetCurrentState().UpdateState();
+        if (!GetAttrubuteManager().IsAlive()) {
+            OnDeath();
+        }
+    }
+
+    public override void HandleMeleeAttack() {
+        foreach (Actor hits in GetCombat().MeleeAttack(GetRigidBody().position, 1.5f, "Player")) {
+            hits.GetAttrubuteManager().ApplyDamage(GetAttrubuteManager().GetAttributes().Damage);
+
+            StartCoroutine(FlashSpriteOnHit(hits.GetComponent<SpriteRenderer>()));
+
+        }
+    }
+
+    public override void HandleRangedAttack() {
+
+    }
+
+    public Player GetPlayer()
+    {
+        return _Player;
+    }
+
+    private void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+
+    private PaintTubeStateMachine _PaintTubeStateMachine;
+
+    private Player _Player;
+}
