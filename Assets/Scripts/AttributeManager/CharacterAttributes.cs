ad89814618eb119e90722026029d9cfe00cde5ff
Justwouter <wouter2312@gmail.com>
2024-01-01 15:22:40 +0100

    Actually figured out how to use reflection for CharacterAttributes

--- Assets/Scripts/AttributeManager/CharacterAttributes.cs
+++ Assets/Scripts/AttributeManager/CharacterAttributes.cs
@@ -1,49 +1,39 @@
+using System;
+using System.Reflection;
+
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CharacterAttributes", menuName = "Brushes/Actor/Attributes")]
+[CreateAssetMenu(fileName = "CharacterAttributes", menuName = "Brushes/Actor/Attributes"), Serializable]
 public class CharacterAttributes : ScriptableObject {
 
     public Sprite DialogueSprite;
     public float InteractionRange;
-
     public Role Type;
-
     public string Name;
-
     public int MaxHealth;
-
     public int CurrentHealth;
-
     public int Damage;
-
     public float Speed;
-
     public float SprintSpeed;
-
     public GameObject Loot;
-
-    public enum Role {
-        Player,
-        Enemy,
-        PaintEnemy,
-        NPC,
-        Static
-    }
 
     public CharacterAttributes Copy() {
         CharacterAttributes newCharacterAttributes = CreateInstance<CharacterAttributes>();
-        
-        newCharacterAttributes.DialogueSprite = DialogueSprite;
-        newCharacterAttributes.InteractionRange = InteractionRange;
-        newCharacterAttributes.Type = Type;
-        newCharacterAttributes.Name = Name;
-        newCharacterAttributes.MaxHealth = MaxHealth;
-        newCharacterAttributes.CurrentHealth = CurrentHealth;
-        newCharacterAttributes.Damage = Damage;
-        newCharacterAttributes.Speed = Speed;
-        newCharacterAttributes.Loot = Loot;
-        newCharacterAttributes.SprintSpeed = SprintSpeed;
+
+        FieldInfo[] fields = GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields) {
+            field.SetValue(newCharacterAttributes, field.GetValue(this));
+        }
 
         return newCharacterAttributes;
     }
+}
+
+public enum Role {
+    Player,
+    Enemy,
+    PaintEnemy,
+    NPC,
+    Static
 }
