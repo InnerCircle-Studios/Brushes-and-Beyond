83a6cd7d45178548f118fdc5c80ea837550e54ec
M0rga-n <verburgnoah@gmail.com>
2024-01-08 06:09:08 +0100

    Health bar

---
+++ Assets/Scripts/Random/Health.cs
@@ -0,0 +1,32 @@
+using System;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Player _Player;
+    public int _numOfHearts;
+
+    public Image[] _hearts;
+    public Sprite _fullHeart;
+    public Sprite _halfHeart;
+    public Sprite _epmtyHearth;
+
+    void Update()
+    {
+        double Health = _Player.GetAttrubuteManager().GetAttributes().CurrentHealth;
+
+        for (int i = 0; i < _hearts.Length; i++)
+        {
+            if (i < Health / 2)
+            {
+                _hearts[i].sprite = _fullHeart;
+            }
+            else
+            {
+                _hearts[i].sprite = _epmtyHearth;
+            }
+        }
+    }
+}
