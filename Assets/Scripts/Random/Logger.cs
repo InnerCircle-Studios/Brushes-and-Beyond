e7bc17883e6f944cd58eb0bf92662319a956ce35
Justwouter <wouter2312@gmail.com>
2024-01-08 17:05:11 +0100

    Better readable color codes

--- Assets/Scripts/Random/Logger.cs
+++ Assets/Scripts/Random/Logger.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public static class Logger {
     public static void Log(string name, string message) {
-        Debug.Log($"<color=#ffffff>[  <color=#5eff33>{name}</color>  ]: " + message+"</color>");
+        Debug.Log($"<color=silver>[  <color=lime>{name}</color>  ]: " + message+"</color>");
     }
 
     public static void LogError(string name, string message) {
-        Debug.LogError($"[ <color=#ff0000>{name}</color>  ]: " + message);
+        Debug.LogError($"<color=white>[ <color=#red>{name}</color>  ]: " + message+"</color>");
     }
 }
