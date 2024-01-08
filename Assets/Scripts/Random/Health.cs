8b95a22cba0a48b6fc2aa60fbedf931678a091d4
Justwouter <wouter2312@gmail.com>
2024-01-08 17:14:38 +0100

    Reset player speed back to normal

--- Assets/Scripts/Random/Health.cs
+++ Assets/Scripts/Random/Health.cs
@@ -11,7 +11,7 @@ public class HealthBar : MonoBehaviour
     public Image[] _hearts;
     public Sprite _fullHeart;
     public Sprite _halfHeart;
-    public Sprite _epmtyHearth;
+    public Sprite _emptyHearth;
 
     void Update()
     {
@@ -25,7 +25,7 @@ public class HealthBar : MonoBehaviour
             }
             else
             {
-                _hearts[i].sprite = _epmtyHearth;
+                _hearts[i].sprite = _emptyHearth;
             }
         }
     }
