e74d21329fc37f140fd7ae3fd3f0329d664528e3
Tackers <83538484+MrTackers@users.noreply.github.com>
2024-01-17 16:56:15 +0100

    Added Settings UI part 1

--- Assets/Scripts/Random/ButtonHoverAnimation.cs
+++ Assets/Scripts/Random/ButtonHoverAnimation.cs
@@ -1,26 +1,40 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro; // Required for accessing TextMeshPro components
 
 public class ButtonHoverAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Animator animator;
+    private RectTransform rectTransform;
+    private TextMeshProUGUI textMeshPro;
+    private Vector3 originalPosition;
 
     void Start()
     {
-        // Get the Animator attached to the button
-        animator = GetComponent<Animator>();
+        // Get the RectTransform component attached to the button
+        rectTransform = GetComponent<RectTransform>();
+        originalPosition = rectTransform.localPosition;
+
+        // Get the TextMeshProUGUI component from the button's children (if it exists)
+        textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // Play hover animation
-        animator.Play("Button1Animation");
-        animator.Play("Button1Idle");
+        // Move the button a bit down
+        rectTransform.localPosition = new Vector3(rectTransform.localPosition.x, rectTransform.localPosition.y - 15, rectTransform.localPosition.z);
+
+        // Make the text bold if the TextMeshProUGUI component exists
+        if (textMeshPro != null)
+            textMeshPro.fontStyle = FontStyles.Bold;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // Play exit animation (if you have one)
-        animator.Play("Button1Idle");
+        // Move the button back to its original position
+        rectTransform.localPosition = originalPosition;
+
+        // Revert the text style to normal if the TextMeshProUGUI component exists
+        if (textMeshPro != null)
+            textMeshPro.fontStyle = FontStyles.Normal;
     }
 }
