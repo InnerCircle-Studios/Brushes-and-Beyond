fff1bd219da2888e8f3f9b5ca539becc5d0f5872
Justwouter <wouter2312@gmail.com>
2024-01-21 15:08:54 +0100

    Added some warning logs

--- Assets/Scripts/Lib/Logger.cs
+++ Assets/Scripts/Lib/Logger.cs
@@ -5,6 +5,10 @@ public static class Logger {
         Debug.Log($"<color=silver>[  <color=lime>{name}</color>  ]: " + message + "</color>");
     }
 
+    public static void LogWarning(string name, string message) {
+        Debug.Log($"<color=silver>[  <color=orange>{name}</color>  ]: " + message + "</color>");
+    }
+
     public static void LogError(string name, string message) {
         Debug.Log($"<color=silver>[  <color=red>{name}</color>  ]: " + message + "</color>");
     }
